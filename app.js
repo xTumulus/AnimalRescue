9130df73439d0c914cf5c634c1d882d76fc61034
xTumulus <logankitch@gmail.com>
2017-11-16 23:10:13 -0700

    imported animal model

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/animalDB',{useMongoClient:true});
+require('./models/animal');
 
 var routes = require('./routes/index');
 
