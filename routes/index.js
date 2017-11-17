b4e4576989f664ec889f2b31c6b5280e88c9b405
Kitchen <KitchenLD@ad.mtc.byu.edu>
2017-11-17 08:00:16 -0700

    working on db connection

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,45 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var animal = mongoose.model('animal');
+
+// var animalSchema = new mongoose.Schema({
+//   name: String,
+//   health: {type: Number, default: 100},
+//   happiness: {type: Number, default: 100},
+// });
+
+// animalSchema.methods.feed = function(cb) {
+//   this.health += 33;
+//   if(this.health > 100) {
+//     this.health = 100;
+//   }
+//   this.save(cb);
+// };
+
+// animalSchema.methods.play = function(cb) {
+//   this.happiness += 33;
+//   if(this.happiness > 100) {
+//     this.happiness = 100;
+//   }
+//   this.save(cb);
+// };
+
+// animalSchema.methods.decrease = function(cb) {
+//   this.happiness -= 33;
+//   if(this.happiness < 1) {
+//     console.log('animal died in model');
+//     //some function to kill it?
+//   }
+//   this.health -= 33;
+//   if(this.health < 1) {
+//     console.log('animal died in model');
+//     //some function to kill it?
+//   }
+//   this.save(cb);
+// };
+
+// mongoose.model('animal', animalSchema);
+var animal = mongoose.model('animal', animalSchema);
 
 // router.get('/animals', function(req, res, next) {
 //   animal.find(function(err, comments){
